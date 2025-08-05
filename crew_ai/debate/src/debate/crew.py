f5d0f343ce313d833a2728d9fbe26b0b970189f0
Vitor Augusto <vitorlealeng@gmail.com>
2025-08-05 19:20:32 -0300

    crewai: debate multi-agent flow

---
+++ crew_ai/debate/src/debate/crew.py
@@ -0,0 +1,47 @@
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+from crewai.agents.agent_builder.base_agent import BaseAgent
+from typing import List
+
+@CrewBase
+class Debate():
+    """Debate crew"""
+
+    agents: List[BaseAgent]
+    tasks: List[Task]
+
+    @agent
+    def debater(self) -> Agent:
+        return Agent(
+            config=self.agents_config['debater'],
+            verbose=True
+        )
+
+    @agent
+    def judge(self) -> Agent:
+        return Agent(
+            config=self.agents_config['judge'],
+            verbose=True
+        )
+
+    @task
+    def propose(self) -> Task:
+        return Task(config=self.tasks_config['propose'])
+
+    @task
+    def oppose(self) -> Task:
+        return Task(config=self.tasks_config['oppose'])
+    
+    @task
+    def decide(self) -> Task:
+        return Task(config=self.tasks_config['decide'])
+
+    @crew
+    def crew(self) -> Crew:
+        """Creates the Debate crew"""
+        return Crew(
+            agents=self.agents, # Automatically created by the @agent decorator
+            tasks=self.tasks, # Automatically created by the @task decorator
+            process=Process.sequential,  # You can change this to Process.hierarchical if needed
+            verbose=True
+        )
